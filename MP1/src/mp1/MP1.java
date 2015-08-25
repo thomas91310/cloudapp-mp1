ebb3508428b4081ecfaab4e54d2c5d8b68a5dbb7
Thomas <thomas.theissier@gmail.com>
2015-08-25 00:16:59 -0400

    Read through lines of input + tokenize the line into separate words

--- MP1/src/mp1/MP1.java
+++ MP1/src/mp1/MP1.java
@@ -1,3 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mp1;
+
+/**
+ *
+ * @author ttheissier
+ */
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
